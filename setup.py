65f6022ec6bf634d2a7b123a21f074edee663101
Jhon Yana <ramstojh@gmail.com>
2022-03-18 01:03:45 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='kanchay',
+      version='1.9',
+      description='Tools based on lightkurve, starspot and exoplanet codes to measure rotational periods',
+      url='https://github.com/ramstojh/kanchay',
+      author='Jhon Yana',
+      author_email='ramstojh@alumni.usp.br',
+      license='MIT',
+      packages=['kanchay'],
+      #install_requires=['matplotlib', 'tqdm', 'numpy', 'pandas', 'pymc3', 'theano', 'exoplanet'],
+      zip_safe=False)
